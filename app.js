a648a6db9cbe102877ff58685253a9c31bc69811
Karl Mark Balagtey <karl.balagtey@gmail.com>
2019-08-09 16:03:40 +0100

    Authentication, fetching creating paginating posts

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const graphqlHttp = require('express-graphql');
 
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
 
 require('dotenv').config();
 
@@ -48,8 +49,13 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
+
+app.use(auth);
 
 app.use(
     '/graphql', 
